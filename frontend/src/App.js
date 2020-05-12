802662ccaa3a8ae560b05adbc5fc386b9cdaabda
Austin Wang <austin.wang0311@gmail.com>
2020-05-12 00:21:56 -0400

    Change alert to modal, add loading icon, update pagination

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
 import axios from 'axios';
-import {Container, Row, Col, Button} from 'react-bootstrap';
+import {Container, Row, Col, Button, Modal, Spinner} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
 import BootstrapTable from 'react-bootstrap-table-next';
+import 'react-bootstrap-table2-paginator/dist/react-bootstrap-table2-paginator.min.css';
 import paginationFactory from 'react-bootstrap-table2-paginator';
 
 // Define columns here
@@ -30,11 +31,30 @@ const selectRow = {
   clickToSelect: true, // enables clicking the row to select the item
 };
 
+const paginationOpts = {
+  sizePerPage: 25,
+  sizePerPageList: [
+    {
+      text: '10', value: 10
+    }, {
+      text: '25', value: 25
+    }, {
+      text: '50', value: 25
+    }, {
+      text: '100', value: 100
+    }
+  ]
+};
+
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      contributions: []
+      contributions: [],
+      showModal: false,
+      modalTitle: "",
+      modalBody: "",
+      loading: true
     }
   }
 
@@ -42,26 +62,45 @@ class App extends React.Component {
   submitData = () => {
     let uuids = this.table.selectionContext.selected; // we bind this.table in the ref attribute of the BootstrapTable element below
     if(uuids.length === 0) {
-      // TODO change alert to something a little nicer
-      alert("Please select at least one entry");
+      this.setState({
+        showModal: true,
+        modalTitle: "No selection made",
+        modalBody: "Please select at least one entry before pressing submit."
+      });
     }
     else {
       axios.post('/contributions/clean', {
         data: uuids
       })
-      .then(() => {
-        // TODO change alert to something a little nicer
-        alert("Submitted!")
-        this.getContributions();
+      .then((response) => {
+        if(response.status === 200) {
+          this.setState({
+            showModal: true,
+            modalTitle: "Success",
+            modalBody: "Your submission has been processed successfully!"
+          });
+          this.getContributions();
+        }
+        else {
+          this.setState({
+            showModal: true,
+            modalTitle: "Error",
+            modalBody: "There was an error with your submission. Please try again later or refresh for a new set of contributions."
+          });
+        }
       });
     }
   }
 
   // makes a request to get a set of matched contributions and updates the state
   getContributions = async () => {
+    this.setState({
+      loading: true
+    });
     axios.get('/contributions/raw').then((response) => {
       this.setState({
-        contributions: response.data.data
+        contributions: response.data.data,
+        loading: false
       });
     });
   }
@@ -70,7 +109,7 @@ class App extends React.Component {
     this.getContributions();
   }
 
-  render() {
+  render() {    
     return (
       <Container>
         <Row>
@@ -84,18 +123,37 @@ class App extends React.Component {
           </Col>
         </Row>
         <Row>
-          <Col>
-            <BootstrapTable
-              ref={ t => this.table = t }
-              keyField='id'
-              data={ this.state.contributions }
-              columns={ columns }
-              selectRow={ selectRow }
-              pagination={ paginationFactory() }
-            />
-          </Col>
+            {this.state.loading ? 
+              <Col className='text-center'>
+                <Spinner animation="border" variant="primary" role="status">
+                  <span className="sr-only">Loading...</span>
+                </Spinner>
+              </Col>
+              :
+              <Col>
+                <BootstrapTable
+                  ref={ t => this.table = t }
+                  keyField='id'
+                  data={ this.state.contributions }
+                  columns={ columns }
+                  selectRow={ selectRow }
+                  pagination={ paginationFactory(paginationOpts) }
+                />
+                <Button className='float-right' variant='primary' onClick={ this.submitData }>Submit</Button>
+              </Col>
+            }
         </Row>
-        <Button className='float-right' variant='primary' onClick={ this.submitData }>Submit</Button>
+        <Modal show={ this.state.showModal } onHide={ () => this.setState({showModal: false}) }>
+          <Modal.Header closeButton>
+            <Modal.Title>{ this.state.modalTitle }</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>{ this.state.modalBody }</Modal.Body>
+          <Modal.Footer>
+            <Button variant="primary" onClick={ () => this.setState({showModal: false}) }>
+              Close
+            </Button>
+          </Modal.Footer>
+        </Modal>
       </Container>
     );
   }
